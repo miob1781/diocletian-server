3cc40090da1ff4c68dde19619a29bde827e00627
Michael Oberst <mioberst@gmx.de>
2022-10-19 14:21:33 +0200

    add route to delete game

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -63,4 +63,17 @@ router.put("/:id", (req, res, next) => {
         })
 })
 
+router.delete("/:id", (req, res, next) => {
+    const { id } = req.params
+
+    Game.findByIdAndRemove(id)
+        .then(() => {
+            res.status(204).send()
+        })
+        .catch(err => {
+            console.log("Error while deleting game: ", err);
+            next(err);
+        })
+})
+
 module.exports = router
